8ba284981e12b690dcaf32d5f5c504697a1876fd
Eric A. Scuccimarra <skooch@gmail.com>
2022-01-16 07:17:15 +0100

    Fixing errors

--- object_detection/configs/convnext/cascade_mask_rcnn_convnext_base_patch4_window7_mstrain_480-800_giou_4conv1f_adamw_3x_coco_in22k.py
+++ object_detection/configs/convnext/cascade_mask_rcnn_convnext_base_patch4_window7_mstrain_480-800_giou_4conv1f_adamw_3x_coco_in22k.py
@@ -130,7 +130,7 @@ test_pipeline = [
     dict(
         type='MultiScaleFlipAug',
         img_scale=[(480,  480), (640, 640), (864, 864)],
-        flip=Trur,
+        flip=True,
         transforms=[
             dict(type='Resize', keep_ratio=True),
             dict(type='RandomFlip'),
