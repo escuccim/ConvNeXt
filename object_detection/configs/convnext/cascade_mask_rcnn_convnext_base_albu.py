794faca91340e3ab0e79a9e250e2bdeec4bf591c
Eric A. Scuccimarra <skooch@gmail.com>
2022-01-16 16:16:40 +0100

    Enable apex

--- object_detection/configs/convnext/cascade_mask_rcnn_convnext_base_albu.py
+++ object_detection/configs/convnext/cascade_mask_rcnn_convnext_base_albu.py
@@ -433,12 +433,12 @@ resume_from = None
 workflow = [('train', 1)]
 
 # do not use mmdet version fp16
-# fp16 = None
-# optimizer_config = dict(
-#     type="DistOptimizerHook",
-#     update_interval=1,
-#     grad_clip=None,
-#     coalesce=True,
-#     bucket_size_mb=-1,
-#     use_fp16=True,
-# )
+fp16 = None
+optimizer_config = dict(
+    type="DistOptimizerHook",
+    update_interval=1,
+    grad_clip=None,
+    coalesce=True,
+    bucket_size_mb=-1,
+    use_fp16=True,
+)
