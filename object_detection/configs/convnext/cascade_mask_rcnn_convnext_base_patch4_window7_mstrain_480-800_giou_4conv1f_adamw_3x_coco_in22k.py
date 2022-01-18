944f2d471df10e8d1b4d5378d896fe0b8eff4ca9
Eric A. Scuccimarra <skooch@gmail.com>
2022-01-18 05:51:50 +0100

    Increasing learning rate

--- object_detection/configs/convnext/cascade_mask_rcnn_convnext_base_patch4_window7_mstrain_480-800_giou_4conv1f_adamw_3x_coco_in22k.py
+++ object_detection/configs/convnext/cascade_mask_rcnn_convnext_base_patch4_window7_mstrain_480-800_giou_4conv1f_adamw_3x_coco_in22k.py
@@ -249,7 +249,7 @@ test_pipeline = [
 data = dict(train=dict(pipeline=train_pipeline))
 
 optimizer = dict(constructor='LearningRateDecayOptimizerConstructor', _delete_=True, type='AdamW', 
-                 lr=0.0001, betas=(0.9, 0.999), weight_decay=0.05,
+                 lr=0.001, betas=(0.9, 0.999), weight_decay=0.05,
                  paramwise_cfg={'decay_rate': 0.8,
                                 'decay_type': 'layer_wise',
                                 'num_layers': 12})
